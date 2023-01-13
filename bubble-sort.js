3c81c6e56f470eac571fea9501f770e2c4ab1d73
kevin ocansey <kevinocansey11@gmail.com>
2023-01-13 22:30:13 +0000

    completed

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,30 @@
 
 function bubbleSort(arr) {
+  // Iterate through the array
+  let swap = false
 
-    // Iterate through the array
+  for(let i = 0; i < arr.length ; i++){
+    // If the current value is greater than its neighbor to the right
+    let value =  arr[i]
+    let next = arr[i+1]
+    if(value > next){
+      // Swap those values
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+      swap = true
 
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
+      // Do not move this console.log
+      console.log(arr.join(","));
+    }
 
-        // Do not move this console.log
-        console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
+  }
+  // If you get to the end of the array and no swaps have occurred, return
+  if (!swap) {
+    return arr;
+  } else {
     // Otherwise, repeat from the beginning
-
+    bubbleSort(arr);
+  }
+  return arr;
 }
 
 module.exports = bubbleSort;
